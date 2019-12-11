00a27fd2ba03b180ddd2ebe60e470f07533b6c58
Peishan <171846z@mymail.nyp.edu.sg>
2019-12-11 21:21:51 +0100

    Update on professor login

--- src/pages/professorlogin/professorlogin.ts
+++ src/pages/professorlogin/professorlogin.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { UltimatumPage } from '../ultimatum/ultimatum';
-
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ProfessorHomePage } from '../professor-home/professor-home';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import * as Crypto from "crypto-browserify";
 /**
  * Generated class for the ProfessorloginPage page.
  *
@@ -15,20 +18,69 @@ import { UltimatumPage } from '../ultimatum/ultimatum';
   templateUrl: 'professorlogin.html',
 })
 export class ProfessorloginPage {
+  Username = '';
+  UserPassword = '';
+  submitted:any;
+  itemDoc:any;
+  item:any;
+  validations_form: FormGroup;
+  errorMessage = '';
+  constructor(public navCtrl: NavController,
+    public afs: AngularFirestore,
+    private _FB: FormBuilder,
+    public navParams: NavParams,
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    ) {
+      this.validations_form = this._FB.group({
+        'email'        : ['', Validators.required],
+        'password'     : ['', Validators.required]
+     });
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfessorloginPage');
   }
 
-  Next(){
+  Next() {
 
+    // retrieve salt value from database
+    // Hash user's password and salt value = hashed password
+    // compare the hashed password from db
+    this.itemDoc = this.afs.collection<any>('ProfessorAccount');
+    this.item = this.itemDoc.valueChanges();
+
+    this.item.subscribe(res=>{
+      console.log(res);
+      for (let p=0;p<res.length;p++){
+        if (res[p]==undefined || res[p]==null){
+          console.log("BYE");
+        }
+        else{
+          // there should be only currenty one account, hence 1 passwordHash & passwordSalt
+          let userpwd=this.validations_form.get('password').value;
+          var userAttemptPasswordHash = this.hashing(userpwd + res[p].PasswordSalt);
+
+          if (userAttemptPasswordHash == res[p].PasswordHash && this.Username == res[p].Username) {
+            console.log("User has entered the correct password!");
+            this.navCtrl.setRoot(ProfessorHomePage);
+          }
+          else {
+            this.errorMessage = "Wrong password or username";
+            console.log("User has entered the wrong password or username!");
+          }
+        }
+
+      }
+
+    })
   }
 
   back(){
     this.navCtrl.setRoot(UltimatumPage);
   }
 
+  hashing = function(length){
+    return Crypto.createHash('sha256').update(length).digest('base64');
+  };
 }
