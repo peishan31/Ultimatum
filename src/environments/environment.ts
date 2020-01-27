4ab768ee6dbbde05f68ac548fcfed45c2f05e533
Peishan <171846z@mymail.nyp.edu.sg>
2020-01-27 22:48:41 +0100

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyAE1UAeSqFLk8KVQ-rJFz6Fi_hul4ZQjmg",
-//   authDomain: "ultimatum-f9099.firebaseapp.com",
-//   databaseURL: "https://ultimatum-f9099.firebaseio.com",
-//   projectId: "ultimatum-f9099",
-//   storageBucket: "ultimatum-f9099.appspot.com",
-//   messagingSenderId: "42716076245",
-//   appId: "1:42716076245:web:00981da86171725bf36a5c",
-//   measurementId: "G-6RQ61HDYPW"
-// };
+export const firebaseConfig = {
+  apiKey: "AIzaSyAE1UAeSqFLk8KVQ-rJFz6Fi_hul4ZQjmg",
+  authDomain: "ultimatum-f9099.firebaseapp.com",
+  databaseURL: "https://ultimatum-f9099.firebaseio.com",
+  projectId: "ultimatum-f9099",
+  storageBucket: "ultimatum-f9099.appspot.com",
+  messagingSenderId: "42716076245",
+  appId: "1:42716076245:web:00981da86171725bf36a5c",
+  measurementId: "G-6RQ61HDYPW"
+};
  // Your web app's Firebase configuration
 // export const firebaseConfig = {
 //    apiKey: "AIzaSyCev4ycC2FpUXua-nJYVLoeoTPr0TZHcbE",
@@ -29,16 +29,16 @@
 //    appId: "1:260274564770:web:db127062acae7446bf71fb"
 //  };
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyDMSWJXbWuK5Da3HLcWIAjoIYJ7IxvOVoo",
-  authDomain: "ultimatum-5c5e6.firebaseapp.com",
-  databaseURL: "https://ultimatum-5c5e6.firebaseio.com",
-  projectId: "ultimatum-5c5e6",
-  storageBucket: "ultimatum-5c5e6.appspot.com",
-  messagingSenderId: "573323748620",
-  appId: "1:573323748620:web:6445c64b07f3d34d181529",
-  measurementId: "G-YRQ7HXHNR1"
-};
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyDMSWJXbWuK5Da3HLcWIAjoIYJ7IxvOVoo",
+//   authDomain: "ultimatum-5c5e6.firebaseapp.com",
+//   databaseURL: "https://ultimatum-5c5e6.firebaseio.com",
+//   projectId: "ultimatum-5c5e6",
+//   storageBucket: "ultimatum-5c5e6.appspot.com",
+//   messagingSenderId: "573323748620",
+//   appId: "1:573323748620:web:6445c64b07f3d34d181529",
+//   measurementId: "G-YRQ7HXHNR1"
+// };
 
 // export const firebaseConfig = {
 //   apiKey: "AIzaSyCeZaKyIEK37DPBgfrAL5ITTML5JR-R0Rg",
